6fc75bf6d8e386825036a67311cab172b8f9b9b1
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-12 08:40:18 +0530

    Added:Ability to retrive data from payroll service data base

---
+++ src/main/java/com/bridzelabz/jdbc/EmployeePayrollData.java
@@ -0,0 +1,16 @@
+package com.bridzelabz.jdbc;
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+    public LocalDate start;
+
+    public EmployeePayrollData(int id, String name, double salary, LocalDate start) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.start = start;
+    }
+}
---
+++ src/main/java/com/bridzelabz/jdbc/EmployeePayrollService.java
@@ -0,0 +1,27 @@
+package com.bridzelabz.jdbc;
+import java.util.List;
+
+import java.util.List;
+
+public class EmployeePayrollService {
+
+    EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+
+    public enum IOService {
+        DB_IO
+    }
+
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+    }
+
+    /**
+     * Purpose : To get the list of employee payroll from the database
+     */
+    public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.DB_IO))
+            this.employeePayrollList = employeePayrollDBService.readData();
+        return this.employeePayrollList;
+    }
+}
